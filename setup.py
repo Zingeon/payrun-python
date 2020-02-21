2753cc371e13f5813beaf7f8d287f02435f183ec
Andrii Pushkar <andrii.pushkar.consultant@consumer.org>
2020-02-21 18:27:59 +0200

    changed setup.py and renamed package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Python SDK for PayRun API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Zingeon/payrun-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
