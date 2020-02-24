619ea5d555e0e475481589b2576085b08847f2e8
Andrii Pushkar <andrii.pushkar.consultant@consumer.org>
2020-02-24 15:14:11 +0200

    updated init files for modules

--- payrun/__init__.py
+++ payrun/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 consumer_key = None
 consumer_secret = None
 api_base = "https://api.test.payrun.io"
---
+++ payrun/api/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import, division, print_function
+
+from payrun.api.employers import Employers
